20a5f33c5f8b1b1ab3c52e8b35f4013b7511d51c
IanPatrickMTan <ianparicktan@gmail.com>
2024-10-21 00:35:55 -0400

    hmm tested and seems to be at run without error on forward pass but the probabilities are all almost 50/50 which is a bit concerning

--- src/hmm.py
+++ src/hmm.py
@@ -4,7 +4,7 @@ import torch as pt
 def settle_state(transition):
 
     eigvals, eigvecs = pt.linalg.eig(transition)
-    stationary_idx = torch.argmin(torch.abs(eigvals - 1))
+    stationary_idx = pt.argmin(pt.abs(eigvals - 1))
     stationary_vec = eigvecs[:, stationary_idx].real
     stationary_dist = stationary_vec / stationary_vec.sum()
 
@@ -15,52 +15,61 @@ def settle_state(transition):
 class HMM(pt.nn.Module):
 
 
-    def __init__(self, transition=None, emission=None, state_amt=None, symbol_amt=None):
+    def __init__(self, transition=None, emission=None, state_amnt=None, symbol_amnt=None):
 
         super().__init__()
 
         transition_missing = transition == None
         emission_missing = emission == None
-        state_amt_missing = state_amt == None
-        symbol_amt_missing = symbol_amt == None
+        state_amnt_missing = state_amnt == None
+        symbol_amnt_missing = symbol_amnt == None
 
-        if transition_missing and state_amt_missing:
-            raise Exception("You must state either transition or state_amt.")
-        if emission_missing and (state_amt_missing or symbol_amt_missing):
-            raise Exception("You must state either transition or state_amt.")
+        if transition_missing and state_amnt_missing:
+            raise Exception("You must state either transition or state_amnt.")
+        if emission_missing and (state_amnt_missing or symbol_amnt_missing):
+            raise Exception("You must state either transition or state_amnt.")
 
         if transition == None:
-            transition = pt.randn(state_amt, state_amt)
+            transition = pt.randn(state_amnt, state_amnt)
         if emission == None:
-            emission = pt.randn(state_amt, symbol_amt)
+            emission = pt.randn(symbol_amnt, state_amnt)
 
         self.transition = pt.nn.Parameter(transition)
         self.emission = pt.nn.Parameter(emission)
-        self.state_amt, self.symbol_amt = emission.shape
-        self.initial_state = pt.eye(self.state_amt)
+        self.symbol_amnt, self.state_amnt = emission.shape
+
+        self._normalize()
+
+
+    def _normalize(self):
+        with pt.no_grad():
+            self.transition /= self.transition.sum(axis=0)
+            self.emission /= self.emission.sum(axis=0)
 
 
     def forward(self, x_seq, steps=1, state=None):
 
         if state == None:
-            state = settle_state(self.transition)
+            state = settle_state(self.transition)[None, :]
 
-        x = x_seq[0]
-        symbol_probs = self.emission[:, x]
-        state = state * symbol_probs)
-        state /= state.sum()
+        state = state.T
 
-        for x in x_seq[1:]:
-            symbol_probs = self.emission[:, x]
-            state = self.transition @ state * symbol_probs
-            state /= state.sum()
+        for x in x_seq.T:
+            state = self.emission[x].T * state
+            state /= state.sum(axis=0)
+            state = self.transition @ state
 
-        pred = np.zeros(steps, self.symbol_amt)
-        pred[0] = self.emission @ state
+        pred = pt.zeros(len(x_seq), steps, self.symbol_amnt)
+        pred[:, 0] = (self.emission @ state).T
 
         for i in range(1, steps):
             state = self.transition @ state
-            state /= state.sum()
-            pred[i] = self.emission @ state
+            pred[:, i] = (self.emission @ state).T
         
         return pred
+
+
+if __name__ == "__main__":
+    m = HMM(state_amnt=3, symbol_amnt=2)
+    x = pt.randint(2, (5, 4))
+    y = m(x)
